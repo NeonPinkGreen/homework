b6e0dbec5b6cd8c04fae64f33f9fcd954c564ce0
Volodymyr Kliushnikov <v.kliushnikov@gmail.com>
2022-07-07 00:49:22 +0300

    hw_02

--- hw_02/js/my.js
+++ hw_02/js/my.js
@@ -2,6 +2,6 @@ let yearOfBirth = prompt("Year of birth:");
 let firstName = prompt("Name:");
 let lastName = prompt("Surname:");
 const CURRENT_YEAR = 2022;
-parseInt(yearOfBirth);
+yearOfBirth = parseInt(yearOfBirth, 10);
 let age = CURRENT_YEAR - yearOfBirth;
 alert("User Bio: " + firstName + " " + lastName + ", " + age + " years old.");
