7b5e4760388673b0594bae54b103f9bff5dc4f5c
Volodymyr Kliushnikov <v.kliushnikov@gmail.com>
2022-08-11 23:37:35 +0300

    НІЧОГО НЕ ПРАЦЮЄ ВИЯВИТИ ПРИЧИНУ САМОСТІЙНО НЕ ВДАЛОСЯ hw_11

---
+++ hw_05/js/my.js
@@ -0,0 +1,58 @@
+const START = confirm(`Tell me the three most important words 💚`);
+
+if (START) {
+    const wordCount = 3;
+    let finalString = ``;
+
+    for (let i = 1; i <= wordCount; i++){
+        let importantWord,
+            wordHasNumber;
+
+        do {
+            importantWord = prompt(`Enter a word #${i}:`);
+
+            if (importantWord) importantWord = importantWord.trim();
+            wordHasNumber = false;
+
+            for (let j = 0; j < importantWord.length; j++){
+                let currentLetter = importantWord[j];
+
+                if ( !isNaN(+currentLetter) ){
+                    wordHasNumber = true;
+                    break;
+                }
+            }
+
+        } while (!importantWord
+        || wordHasNumber);
+
+        let tool;
+
+        do{
+            tool = prompt(`Choose formatting: uppercase, lowercase or capitalize`);
+
+            if(tool) tool = tool.replaceAll(` `, ``).toLowerCase();
+
+        } while (!tool || tool !== 'uppercase'
+        && tool !== 'lowercase'
+        && tool !== 'capitalize');
+
+        switch (tool) {
+            case 'uppercase':
+                importantWord = importantWord.toUpperCase();
+                break;
+
+            case 'lowercase':
+                importantWord = importantWord.toLowerCase();
+                break;
+
+            case 'capitalize':
+                importantWord = importantWord[0].toUpperCase() + importantWord.slice(1).toLowerCase();
+                break;
+        }
+        finalString += importantWord;
+        finalString += i === wordCount ? `!` : ` `;
+    }
+
+    console.log(finalString);
+}
---
+++ hw_11/js/my.js
@@ -0,0 +1,46 @@
+const products = [
+    ['apple',10],
+    ['banana',8],
+    ['mango',20],
+    ['grape',18]
+]
+
+const getPrice = (products, seasonFunc) => {
+    const copiedProducts = [...products];
+    let sum = 0;
+debugger
+    if (seasonFunc) {
+        switch(seasonFunc){
+            case 'summerValue':
+                for (let i = 0; i < copiedProducts.length; i++) {
+                    summerValue(copiedProducts[i][1]);
+                }
+                return sum;
+
+            case 'winterValue':
+                for (let i = 0; i < copiedProducts.length; i++) {
+                    winterValue(copiedProducts[i][1]);
+                    sum += copiedProducts[i][1];
+                }
+                return sum;
+        }
+    } else {
+        for (let i = 0; i < copiedProducts.length; i++) {
+            sum += copiedProducts[i][1];
+        }
+    }
+}
+
+function summerValue(value){
+    return value*0.8;
+}
+
+function winterValue(value){
+    return value*2;
+}
+
+console.log(getPrice(products, summerValue));
+
+console.log(getPrice(products, winterValue));
+
+console.log(getPrice(products));
