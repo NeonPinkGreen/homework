418e332ae91a35cd878264e93b50148fef6186dc
Volodymyr Kliushnikov <v.kliushnikov@gmail.com>
2022-08-24 23:56:59 +0300

    hw_15

--- hw_15/script.js
+++ hw_15/script.js
@@ -106,82 +106,73 @@ class User{
 		return `<div class="user">
             <div class="user__info">
                 <div class="user__info--data">
-                    <img src="images/users/${this[`img`]}.png" alt="${this[`name`]}" height="50">
+                    <img src="images/users/${this.img}.png" alt="${this.name}" height="50">
                     <div class="user__naming">
-                        <p>Name: <b>${this[`name`]}</b></p>
-                        <p>Age: <b>${this[`age`]}</b></p>
+                        <p>Name: <b>${this.name}</b></p>
+                        <p>Age: <b>${this.age}</b></p>
                     </div>
                 </div>
                 <div class="user__info--role student">
-                    <img src="images/roles/${this['role']}.png" alt="${this['role']}" height="25">
-                    <p>${this['role']}</p>
-                </div>` + this.renderCourses() +
-	`</div>
+                    <img src="images/roles/${this.role}.png" alt="${this.role}" height="25">
+                    <p>${this.role}</p>
+                </div>
+			</div>${this.courses ? this.renderCourses() : ``}
         </div>`;
 
-		//
-		// const render = [];
-		//
-		// for (let key in this) {
-		// 	switch (key){
-		// 		case `img`: render.push(`<div class="user">
-        //     								<div class="user__info">
-        //     									<div class="user__info--data">
-		// 											<img src="images/users/${this[key]}.png" alt="${this[key]}" height="50">`);
-		// 			continue;
-		//
-		// 		case `name`: render.push(`<div class="user__naming">
-		// 									<p>${key}: <b>${this[key]}</b>`);
-		// 			continue;
-		//
-		// 		case `age`: render.push(`<p>${key}: <b>${this[key]}</b>
-		// 									</div>
-		// 								</div>`);
-		// 			continue;
-		//
-		// 		case `role`: render.push(`<div class="user__info--role student">
-		// 									<img src="images/roles/${this[key]}.png" alt="${this[key]}" height="25">
-		// 										<p>${this[key]}</p>
-		// 								</div>
-		// 							</div>
-		// 						</div>`);
-		// 		return render.join(``);
-		// 	}
-		// }
-		//
-		// return render;
 	}
 
 	renderCourses(){
-		let coursesResult = users.find(item => item.courses === courses); // {} : undefined
+		const coursesRender = this.courses
+			.map(course => `<p class="user__courses--course ${this.role}">
+								${course.title}
+								<span class="satisfactory">${course.mark}</span>
+							</p>`)
+			.join(``);
 
-		return coursesResult ? `<div class="user__courses">
-			<p class="user__courses--course student">${this['courses'][`title`]} <span class="satisfactory">satisfactory</span></p>
-		<p class="user__courses--course student">Java Enterprise <span class="excellent">excellent</span></p>
-		</div>` : ``;
+		return `<div class="user__courses">${coursesRender}</div>`;
 	}
 }
 
 class Student extends User{
-	constructor(){
-		super();
+	constructor(user){
+		super(user);
 	}
 }
 
 class Lector extends User{
-	constructor(){
-		super();
+	constructor(user){
+		super(user);
 	}
 
-	renderCourses(){}
+	renderCourses(){
+		const coursesRender = this.courses
+			.map(course => `<div class="user__courses--course ${this.role}">
+                    <p>Title: <b>${course.title}</b></p>
+                    <p>Lector's score: <span class="very-good">${course.score}</span></p>
+                    <p>Average student's score: <span class="very-good">${course.studentsScore}</span></p>
+                </div>`)
+			.join(``);
+
+		return `<div class="user__courses admin--info">${coursesRender}</div>`;
+	}
 }
 
 class Admin extends User{
-	constructor(){
-		super();
+	constructor(user){
+		super(user);
 	}
 
-	renderCourses(){}
+	renderCourses(){
+		const coursesRender = this.courses
+			.map(course => `<div class="user__courses--course ${this.role}">
+                    <p>Title: <b>${course.title}</b></p>
+                    <p>Admin's score: <span class="satisfactory">${course.score}</span></p>
+                    <p>Lector: <b>${course.lector}</b></p>
+                </div>`)
+			.join(``);
+
+		return `<div class="user__courses admin--info">${coursesRender}</div>`;
+	}
 }
 
 const USERS_ROLES = {
@@ -191,10 +182,8 @@ const USERS_ROLES = {
 }
 
 let usersClass = users
-	.map(user => USERS_ROLES[users.role] ? USERS_ROLES[users.role](user) : new User(user))
+	.map(user => USERS_ROLES[user.role] ? USERS_ROLES[user.role](user) : new User(user))
 	.map(user => user.render())
 	.join(``);
 
-console.log(usersClass);
-
-document.write(usersClass);
+document.write(`<div class="users">${usersClass}</div>`);
