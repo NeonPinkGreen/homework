a255e80519dadb13ac94ce6e1a57e5184bba2aea
Volodymyr Kliushnikov <v.kliushnikov@gmail.com>
2022-09-03 23:56:32 +0300

    hw_16

---
+++ hw_17/js/my.js
@@ -0,0 +1,9 @@
+const root = document.querySelector(`ul#root`);
+
+const setFirstItemClassByLevel = (level, ul) => {
+    // let LiFirst = ul.querySelector(`li:first-of-type`);
+    // LiFirst.classList.add(`first`);
+    const Lis = ul.querySelectorAll(`li`)
+}
+
+setFirstItemClassByLevel(2, root);
