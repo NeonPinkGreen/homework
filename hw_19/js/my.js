f489edb8438fe72c8e5394d9ac32639ef7411ac8
Volodymyr Kliushnikov <v.kliushnikov@gmail.com>
2022-09-11 01:59:31 +0300

    hw_19

--- hw_19/js/my.js
+++ hw_19/js/my.js
@@ -8,20 +8,54 @@ setCenterPosition();
 
 const movingRight = block => {
     block.style.left = parseInt(block.style.left) + 10 + `px`;
+    block.style.transition = `0.1s`;
 }
 
 const movingLeft = block => {
     block.style.left = parseInt(block.style.left) - 10 + `px`;
+    block.style.transition = `0.1s`;
 }
 
 const movingUp = block => {
     block.style.top = parseInt(block.style.top) - 10 + `px`;
+    block.style.transition = `0.1s`;
 }
 
 const movingDown = block => {
     block.style.top = parseInt(block.style.top) + 10 + `px`;
+    block.style.transition = `0.1s`;
 }
 
+const bounceOut = (direction) => {
+    switch (direction){
+        case `right`: block.style.left = parseInt(block.style.left) - 20 + `px`;
+        block.style.transition = `0.8s`;
+        break;
+
+        case `left`: block.style.left = parseInt(block.style.left) + 20 + `px`;
+        block.style.transition = `0.8s`;
+        break;
+
+        case `top`: block.style.top = parseInt(block.style.top) + 20 + `px`;
+        block.style.transition = `0.8s`;
+        break;
+
+        case `bottom`: block.style.top = parseInt(block.style.top) - 20 + `px`;
+        block.style.transition = `0.8s`;
+        break;
+    }
+
+}
+
+const checkObstacle = (block) => {
+    let obstacleWidth = parseInt(block.style.left) + block.offsetWidth;
+    let obstacleHeight = parseInt(block.style.top) + block.offsetHeight;
+    (document.body.clientWidth < obstacleWidth) ? bounceOut(`right`) : `false`;
+    (parseInt(block.style.left) < 0) ? bounceOut(`left`) : `false`;
+    (document.body.clientHeight < obstacleHeight) ? bounceOut(`bottom`) : `false`;
+    (parseInt(block.style.top) < 0) ? bounceOut(`top`) : `false`;
+
+}
 
 const EVENTS = {
     37: block => movingLeft(block),
@@ -32,4 +66,6 @@ const EVENTS = {
 
 document.addEventListener(`keydown`, event => {
     EVENTS[event.keyCode](block);
+    checkObstacle(block);
 })
+
