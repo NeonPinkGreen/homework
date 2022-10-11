9b6055e943db2784e68780f3e0d75e76cb7bfb48
Volodymyr Kliushnikov <v.kliushnikov@gmail.com>
2022-10-11 22:24:31 +0300

    hw_24

--- hw_24/js/my.js
+++ hw_24/js/my.js
@@ -1,25 +1,58 @@
 const API = `https://raw.githubusercontent.com/brightestsirius/Front-end-Pro-19/master/lesson_27/tesla.json`;
 
-let $colors = $(`#colors`);
-let $car = $(`#car`);
+
+let $colors = $(`#btnsContainer`);
+let $car = $(`#carImg`);
+
+const renderBtns = data => {
+    $(data).each((index, item) => {
+        let $dot = `<button class="btnColor ${item.title}", style="background-color:${item.color}"></button>`;
+        $colors.append($dot);
+    })
+
+    $(data).each((index, item) => {
+        $(`button`).on("click", function () {
+            renderCar(item);
+        })
+    })
+}
+
+const renderCar = item => {
+    $car.attr(`src`, `https://mc-astro.github.io/tesla-roadster-colors/img/${item.img}.jpg`);
+}
 
 $.ajax({
     url: API,
     method: `GET`,
     dataType: "JSON",
-    success: function(colors, car) {
-        $.each(colors, function(i, color) {
-            let $dot = `<span class="dot ${color.title}", style="background-color:${color.color}"></span>`;
-            $colors.append($dot)
-        })
-        $.each(colors, function(i, colors){
-            $(`span`).click(function (){
-                $car.attr(`src`, `https://mc-astro.github.io/tesla-roadster-colors/img/${color.img}.jpg`)
-            })
-        })
+    success: data => {
+        renderBtns(data);
+        renderCar(data[0])
     },
     error: error => console.log(error)
-});
+})
+
+
+//
+// $.ajax({
+//     url: API,
+//     method: `GET`,
+//     dataType: "JSON",
+//     success: function(colors) {
+//         $.each(colors, function(i, color) {
+//             let $dot = `<button class="btnColor ${color.title}", style="background-color:${color.color}"></button>`;
+//             $colors.append($dot)
+//         })
+//
+//     },
+//     error: error => console.log(error)
+// });
+//
+// $.each($colors, function(i, colors) {
+//     $(`#btnColor`).click(function () {
+//         $car.attr(`src`, `https://mc-astro.github.io/tesla-roadster-colors/img/${color.img}.jpg`)
+//     })
+// })
 
 // $(document).ready(function() {
 //     $(`div`).on(`click`, `span`, function(){
