f2e5fc39d4c02becd8d9eaec22624534d74e64b1
Volodymyr Kliushnikov <v.kliushnikov@gmail.com>
2022-10-11 23:04:03 +0300

    hw_24

--- hw_24/js/my.js
+++ hw_24/js/my.js
@@ -6,9 +6,10 @@ let $car = $(`#carImg`);
 
 const renderBtns = data => {
     $(data).each((index, item) => {
-        let $dot = `<button class="btnColor ${item.title}", style="background-color:${item.color}"></button>`;
+        let $dot = $(`<button class="btnColor" style="background-color:${item.color}"></button>`);
         $($dot).on("click", function () {
             renderCar(item);
+            renderTitle(item);
         })
         $colors.append($dot);
     })
@@ -19,13 +20,18 @@ const renderCar = item => {
     $car.attr(`src`, `https://mc-astro.github.io/tesla-roadster-colors/img/${item.img}.jpg`);
 }
 
+const renderTitle = item => {
+    $(`#carTitle`).text(item.title);
+}
+
 $.ajax({
     url: API,
     method: `GET`,
     dataType: "JSON",
     success: data => {
         renderBtns(data);
-        renderCar(data[0])
+        renderCar(data[0]);
+        renderTitle(data[0])
     },
     error: error => console.log(error)
 })
